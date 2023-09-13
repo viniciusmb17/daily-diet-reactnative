3dc871dcb76777b75d36e224461e24b05748f5de
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2023-09-13 15:55:10 -0300

    feat: theme and font

--- App.tsx
+++ App.tsx
@@ -1,5 +1,24 @@
 import { Home } from '@screens/Home'
+import theme from './src/theme'
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold,
+} from '@expo-google-fonts/nunito'
+import { ActivityIndicator, StatusBar, Text } from 'react-native'
+import { ThemeProvider } from 'styled-components'
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <ActivityIndicator />}
+    </ThemeProvider>
+  )
 }
